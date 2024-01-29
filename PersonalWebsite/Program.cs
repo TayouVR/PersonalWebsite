a80c4c38239028dc7b0764e1038b7f1b1c7b57a5
Tayou <git@tayou.org>
2024-01-29 20:11:39 +0100

    experiment - let statiq deploy on its own

--- PersonalWebsite/Program.cs
+++ PersonalWebsite/Program.cs
@@ -2,5 +2,26 @@
 
 public class Program
 {
-    public static async Task<int> Main(string[] args) => await Bootstrapper.Factory.CreateWeb(args).RunAsync();
+    public static KeyValuePair<string, object>[] settings =
+    {
+        new("Host", "tayou.org"),
+        new("Copyright", "Copyright © 2024 Tayou<contact-web@tayou.org> - GPLv3"),
+
+        new("GenerateSearchIndex", false),
+        new("DeployGitHubPages", true),
+        new("LinksUseHttps", true),
+        new("Image", "./images/site-image.avif"),
+        new("GitHubToken", Environment.GetEnvironmentVariable("GITHUB_TOKEN")!),
+    };
+    
+    public static async Task<int> Main(string[] args) => 
+        await Bootstrapper
+            .Factory
+            .CreateWeb(args)
+            .AddSettings(settings)
+            .DeployToGitHubPages(
+                "TayouVR", 
+                "PersonalWebsite", 
+                Config.FromSetting<string>("GitHubToken"))
+            .RunAsync();
 }
